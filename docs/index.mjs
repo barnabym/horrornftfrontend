6bcfa1e29fa4d27debbc6efe0e9a2e83d66c6658
barnaby menage <barnabym@gmail.com>
2022-02-01 22:12:36 +0000

    insex.mjs

---
+++ docs/index.mjs
@@ -0,0 +1,116 @@
+
+const contract_address = "0x519a524192ff7e845f4a4895993f84d9b25f3be3";
+const contract_abi = `[
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkCancelled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkFulfilled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkRequested",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_requestId",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_volume",
+				"type": "uint256"
+			}
+		],
+		"name": "fulfill",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "requestVolumeData",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "requestId",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "volume",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]`;
+
+import Web3 from 'web3';
+
+const start = async () => {
+    try {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+        
+        const NameContract = web3.eth.Contract(contract_abi, contract_address);
+        
+        NameContract.methods.requestVolumeData().call();
+        console.log(NameContract.volume);
+        //NameContract.methods.setName("bitsofcode").send();
+        
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+function preTest() {
+    (async () => await start())();
+}
+
+preTest();
